5d38fa0904d167bf2cd71d6f964badc7bc90ffc1
Bernando Torrez <bernandotorrez4@gmail.com>
2020-10-20 16:50:07 +0700

    update login (4)

--- resources/views/login_layouts/app.blade.php
+++ resources/views/login_layouts/app.blade.php
@@ -28,16 +28,19 @@
     <script src="{{ asset('assets/js/libs/jquery-3.1.1.min.js') }}"></script>
     <script src="{{ asset('bootstrap/js/popper.min.js') }}"></script>
     <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+
+    <!-- END GLOBAL MANDATORY SCRIPTS -->
+    <script src="{{ asset('assets/js/authentication/form-2.js') }}" defer></script>
     <script src="{{ asset('plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
     <script src="{{ asset('assets/js/app.js') }}"></script>
-    
+
     <script>
         $(document).ready(function() {
             App.init();
         });
     </script>
+    
     <script src="{{ asset('assets/js/custom.js') }}"></script>
-    <!-- END GLOBAL MANDATORY SCRIPTS -->
 
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
     @livewireScripts
